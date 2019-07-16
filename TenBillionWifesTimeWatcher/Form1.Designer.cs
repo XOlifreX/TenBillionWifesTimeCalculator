4914e12142093d9c5762761c9e51f2d9f9087445
Olivier Luyckx <olivier.luyckx@student.uantwerpen.be>
2019-07-16 17:09:25 +0200

    Add code to repo

---
+++ TenBillionWifesTimeWatcher/Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace TenBillionWifesTimeWatcher
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.resultBox = new System.Windows.Forms.ListBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.LPStxt = new System.Windows.Forms.TextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.calcBtn = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // resultBox
+            // 
+            this.resultBox.FormattingEnabled = true;
+            this.resultBox.Location = new System.Drawing.Point(6, 19);
+            this.resultBox.Name = "resultBox";
+            this.resultBox.Size = new System.Drawing.Size(367, 186);
+            this.resultBox.TabIndex = 0;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.resultBox);
+            this.groupBox1.Location = new System.Drawing.Point(13, 109);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(379, 210);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Result";
+            // 
+            // LPStxt
+            // 
+            this.LPStxt.Location = new System.Drawing.Point(79, 28);
+            this.LPStxt.Name = "LPStxt";
+            this.LPStxt.Size = new System.Drawing.Size(187, 20);
+            this.LPStxt.TabIndex = 2;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.calcBtn);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Controls.Add(this.LPStxt);
+            this.groupBox2.Location = new System.Drawing.Point(13, 13);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(272, 90);
+            this.groupBox2.TabIndex = 3;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Information";
+            // 
+            // calcBtn
+            // 
+            this.calcBtn.Location = new System.Drawing.Point(129, 54);
+            this.calcBtn.Name = "calcBtn";
+            this.calcBtn.Size = new System.Drawing.Size(75, 23);
+            this.calcBtn.TabIndex = 4;
+            this.calcBtn.Text = "Calculate";
+            this.calcBtn.UseVisualStyleBackColor = true;
+            this.calcBtn.Click += new System.EventHandler(this.calcBtn_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Current LPS:";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.pictureBox1.Image = global::TenBillionWifesTimeWatcher.Properties.Resources._10BWiveslogo;
+            this.pictureBox1.Location = new System.Drawing.Point(292, 3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 100);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(399, 331);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "10B Wives - Time Calculator";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox resultBox;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox LPStxt;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button calcBtn;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ TenBillionWifesTimeWatcher/Form1.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TenBillionWifesTimeWatcher
+{
+    public partial class Form1 : Form
+    {
+
+        private const double MAX_VAL = 18446744073709551615;
+        private const double WoWconstRaise = 30000000000000;
+        private const double oneItteration = 1000000000000000;
+        private double biggerThenVal = 0;
+        private double lps = 0;
+        private double timeNextLevel = 0;
+        private double[] timeNextRoundVal = new double[] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+        private double[] timeNextRoundValResult = new double[10];
+
+        private double timeMaxVal = 0;
+        private double runningLPS = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public bool checkEmptyText()
+        {
+
+            if (LPStxt.Text.Length > 0)
+                return true;
+            else
+                return false;
+
+        }
+
+        public void clearEverything()
+        {
+
+            this.biggerThenVal = 0;
+            this.lps = 0;
+            this.timeNextLevel = 0;
+            this.timeNextRoundVal = new double[] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+            this.timeNextRoundValResult = new double[10];
+            this.timeMaxVal = 0;
+            this.runningLPS = 0;
+
+        }
+
+        public void setLPS(double lps)
+        {
+            this.lps = lps;
+        }
+
+        public void calcAllVals()
+        {
+            
+            //time to next level;
+            this.timeNextLevel = (MAX_VAL / this.lps) / 60;
+
+            //time next round value;
+            bool found = false;
+            double toCheck = 0;
+
+            for(int i = 0; !found; i++) //Look where your LPS is to itterate in the roundVal array later.
+            {
+                if (this.lps < toCheck)
+                    found = true;
+                else
+                    toCheck += oneItteration; ;
+            }
+
+            this.biggerThenVal = toCheck - oneItteration;
+            
+            //Calculate 10 steps ahead of where you are
+            this.runningLPS = this.lps;
+            for (int i = 1; i < 11; i++) //for statement to count the 10 steps
+            {
+                for (double j = this.runningLPS; j < (biggerThenVal + (oneItteration*i)); j += WoWconstRaise) //Checks how long it takes to complete one step
+                {
+                    this.timeNextRoundVal[i-1] += ((((MAX_VAL / j) / 60) / 60) / 24);
+                    this.runningLPS = j;
+                }
+
+                if (i-1 > 0 )
+                {
+                    for (int j = 0; j <= i - 1; j++) //For statement to calculate how long it takes to get to a chosen step from where you currently are
+                        this.timeNextRoundValResult[i - 1] += this.timeNextRoundVal[j];
+                }
+                else
+                    this.timeNextRoundValResult[i-1] = this.timeNextRoundVal[i-1];
+            }
+
+            //Calculate time to Max Value;
+            for(double i = this.lps; i < MAX_VAL; i += WoWconstRaise)
+                this.timeMaxVal += ((((MAX_VAL / i) / 60) / 60) / 24);
+        }
+
+        public void infoToListBox()
+        {
+            //Print function. Prints all the information in a listbox.
+            resultBox.Items.Clear();
+
+            resultBox.Items.Add(String.Format("Time To Next Level: {0:0.00} hours. ({1:00.00} minutes)", this.timeNextLevel/60, this.timeNextLevel));
+
+            for (int i = 0; i < 10; i++)
+            {
+                if (i < this.timeNextRoundVal.Length)
+                    resultBox.Items.Add(String.Format("Time To {0:00.0}: {1:00.00} days, {2:0.00} hours ({3:0.00} days, {4:0.00} hours).",
+                        (this.biggerThenVal + (oneItteration * (i+1))) / 1000000000000000, this.timeNextRoundValResult[i], this.timeNextRoundValResult[i] * 24, this.timeNextRoundVal[i], this.timeNextRoundVal[i] * 24));
+            }
+
+            resultBox.Items.Add(String.Format("Time To MAX_VALUE: {0:000.00} days.", this.timeMaxVal));
+
+        }
+
+        private void calcBtn_Click(object sender, EventArgs e)
+        {
+            if (this.checkEmptyText())
+            {
+
+                this.clearEverything();
+                this.setLPS(Double.Parse(LPStxt.Text));
+
+                this.calcAllVals();
+                this.infoToListBox();
+
+            }
+        }
+
+    }
+}
